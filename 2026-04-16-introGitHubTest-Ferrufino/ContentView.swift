ad7f89cbeff5271d38eb35c254cf5d5d6a6769ec
Omar <oferrufino@sfsu.edu>
2026-04-16 16:33:58 -0700

    Initial Commit

---
+++ 2026-04-16-introGitHubTest-Ferrufino/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  2026-04-16-introGitHubTest-Ferrufino
+//
+//  Created by Yea on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

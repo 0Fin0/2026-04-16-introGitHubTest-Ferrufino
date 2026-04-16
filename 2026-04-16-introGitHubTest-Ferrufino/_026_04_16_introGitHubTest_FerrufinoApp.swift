280287f3960274abc09184b04a9376770ca50768
Omar <oferrufino@sfsu.edu>
2026-04-16 16:33:58 -0700

    Initial Commit

---
+++ 2026-04-16-introGitHubTest-Ferrufino/_026_04_16_introGitHubTest_FerrufinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _026_04_16_introGitHubTest_FerrufinoApp.swift
+//  2026-04-16-introGitHubTest-Ferrufino
+//
+//  Created by Yea on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct _026_04_16_introGitHubTest_FerrufinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
